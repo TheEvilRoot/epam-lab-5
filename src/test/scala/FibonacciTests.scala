20e55fd5ed278facd95073ce9c6d7f4c7330c3b1
TheEvilRoot <robot-creatix@ya.ru>
2020-04-16 12:45:02 +0300

    Fixed tests

--- src/test/scala/FibonacciTests.scala
+++ src/test/scala/FibonacciTests.scala
@@ -1,28 +1,28 @@
-import org.scalatest.FlatSpec
+import org.scalatest.flatspec.AnyFlatSpec
 
-class FibonacciTests extends FlatSpec {
+class FibonacciTests extends AnyFlatSpec {
 
-  "Fibonacci function" should "return list [0, 1, 1, 2] using tail recursion" in {
+  "Find fibonacci number function" should "return list [0, 1, 1, 2] using tail recursion" in {
     assert(Fibonacci.findFibonacci(4, x => Fibonacci.fibonacci_tail(x)) ==
       List(0, 1, 1, 2))
   }
 
-  "Fibonacci function" should "return list [0, 1, 1, 2] using common recursion" in {
+  it should "return list [0, 1, 1, 2] using common recursion" in {
     assert(Fibonacci.findFibonacci(4, Fibonacci.fibonacci) ==
       List(0, 1, 1, 2))
   }
 
-  "Fibonacci functions" should "return same lists" in {
+  it should "return same lists" in {
     assert(Fibonacci.findFibonacci(6, Fibonacci.fibonacci) ==
       Fibonacci.findFibonacci(6, x => Fibonacci.fibonacci_tail(x)))
   }
 
-  "Fibonacci function" should "return empty list with 0 passed" in {
+  it should "return empty list with 0 passed" in {
     assert(Fibonacci.findFibonacci(0, Fibonacci.fibonacci) == List())
     assert(Fibonacci.findFibonacci(0, x => Fibonacci.fibonacci_tail(x)) == List())
   }
 
-  "Fibonacci function" should "return empty list with negative count passed" in {
+  it should "return empty list with negative count passed" in {
     assert(Fibonacci.findFibonacci(-2, Fibonacci.fibonacci) == List())
     assert(Fibonacci.findFibonacci(-2, x => Fibonacci.fibonacci_tail(x)) == List())
   }
